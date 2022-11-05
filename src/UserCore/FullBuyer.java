7de429d1b71ceacbbbd7483bc22da06ec65540b1
Samson Tesfagiorgis <stesfag@purdue.edu>
2022-11-04 20:43:23 -0400

    Updated FullBuyer constructor

--- src/UserCore/FullBuyer.java
+++ src/UserCore/FullBuyer.java
@@ -1,7 +1,23 @@
 package UserCore;
 
-public class FullBuyer {
+public class FullBuyer extends FullUser {
 
 
+    /**
+     * Creates a new Full User instance
+     *
+     * @param userName the name of the user
+     * @param email    the email address assoc. with the user
+     * @param pwd      the password of the user
+     * @throws EmailFormatException when email address are not put in the right format
+     */
+    public FullBuyer(String userName, String email, String pwd) {
+        super(userName, email, pwd, Role.BUYER);
 
+    }
+
+
+    public void viewDashboard() {
+
+    }
 }
