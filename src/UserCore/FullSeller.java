85a039b55ff0cef89c0405dbb0fc0916f8f32481
Samson Tesfagiorgis <stesfag@purdue.edu>
2022-11-11 14:03:58 -0500

    Created IllegalStoreNameException

--- src/UserCore/FullSeller.java
+++ src/UserCore/FullSeller.java
@@ -13,14 +13,13 @@ public class FullSeller extends FullUser {
         stores = new ArrayList<>();
     }
 
-    public boolean createStore(String storeName) {
+    public boolean createStore(String storeName) throws IllegalStoreNameException {
         if (!PublicInformation.findDuplicateStoreName(storeName)) {
             Store store = new Store(storeName, (Seller) getUser());
             stores.add(store);
             return true;
         }
-
-
+        throw new IllegalStoreNameException("This store name is already taken!");
     }
 
     public void viewDashboard() {
