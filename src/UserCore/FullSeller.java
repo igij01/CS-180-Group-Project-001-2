d3fbf1b54d142460fb0054c29e168c4c7d41d8c5
teresawan04 <113661744+teresawan04@users.noreply.github.com>
2022-11-13 14:02:20 -0500

    Update FullSeller.java
    Added searchCustomers method, mostCommonWords method, and sortStoresByPopularity method from PublicInformation

--- src/UserCore/FullSeller.java
+++ src/UserCore/FullSeller.java
@@ -67,7 +67,84 @@ public class FullSeller extends FullUser implements Serializable {
         super.createMessage(buyer, txtFile);
     }
 
+    //returns list of buyers whose usernames contain search
+    public ArrayList<FullBuyer> searchCustomers(String search) {
+        ArrayList<FullBuyer> r = null;
+        for(int i = 0; i < PublicInformation.getListOfBuyers().size(); i++){
+            if(PublicInformation.getListOfBuyers().get(i).getUser().getUserName().contains(search)){
+                r.add(PublicInformation.getListOfBuyers().get(i));
+            }
+        }
+        return r;
+    }
+    
+    //from PublicInformation
+    public static Store[] sortStoresByPopularity(Store[] storeList) {
+        for (int i = 0; i < storeList.length; i++) {
+            for (int j = i + 1; j < storeList.length; j++) {
+                Store temp;
+                if (storeList[i].getCounter() < storeList[j].getCounter()) {
+                    temp = storeList[i];
+                    storeList[i] = storeList[j];
+                    storeList[j] = temp;
+                }
+            }
+        }
+        return storeList;
+    }
+    
+    //finds most common word, if multiple, returns all
+    public ArrayList<String> mostCommonWords() {
+        ArrayList<String> words = null;
+        ArrayList<Integer> occurances = null;
+        ArrayList<String> mcwords= null;
+        //loops through conversations
+        for(int i = 0; i < this.getConversations().size(); i++) {
+            //loops through messages
+            //added conversation getter to FullUser and Conversation, added message getter in Message
+            for(int k = 0; k < this.getConversations().get(i).getConversation().size(); k++){
+                //counts number of words in conversation
+                int wordnum = 1;
+                for(int j = 0; j < (this.getConversations().get(i).getConversation().get(k).getMessage().length()); j++){
+                    if(this.getConversations().get(i).getConversation().get(k).getMessage().charAt(j) == ' '){
+                        wordnum++;
+                    }
+                }
+                //splits each conversation into array of words
+                String[] temp = new String[wordnum];
+                //loops through words
+                for(int j = 0; j < wordnum; j++) {;
+                    //if word already in words arraylist
+                    boolean wiw = false;
+                    for(int l = 0; l < words.size(); l++){
+                        if(temp[j].equals(words.get(l))){
+                            wiw = true;
+                        }
+                    }
+                    if(wiw){
+                        occurances.set(j, occurances.get(j) + 1);
+                    } else {
+                        words.add(temp[j]);
+                        occurances.add(1);
+                    }
+                }
+            }
+        }
 
+        //finds the max
+        int max = 0;
+        for(int i = 0; i < occurances.size(); i++){
+            if(occurances.get(i) > max){
+                mcwords= null;
+                mcwords.add(words.get(i));
+            } else if(occurances.get(i) == max){
+                mcwords.add(words.get(i));
+            }
+        }
+
+        return mcwords;
+    }
+    
     /**
      * @param increasingOrder if true then the sort will go in order from the highest message count to lowest,
      *                        else just in order of history
@@ -81,6 +158,7 @@ public class FullSeller extends FullUser implements Serializable {
             Integer[] buyersMessageCount = store.getMessagingBuyersMessageCount().toArray(new Integer[0]);
             customers.append(PublicInformation.correspondingArraysToString(buyers, buyersMessageCount, increasingOrder));
         }
+        
 
         //MOST POPULAR WORDS GO HERE
 
