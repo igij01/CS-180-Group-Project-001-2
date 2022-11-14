f20a440376e54114bcab043552069857c9ba8bb9
Samson Tesfagiorgis <stesfag@purdue.edu>
2022-11-14 13:58:56 -0500

    Javadoc for exceptions

--- src/UserCore/InvalidPasswordException.java
+++ src/UserCore/InvalidPasswordException.java
@@ -1,10 +1,14 @@
 package UserCore;
 
+/**
+ * InvalidPasswordException
+ * <p>
+ *
+ * throw this when a user tries to login but the password doesn't match the username
+ * @author Samson Tesfagiorgis, 001
+ * @version 11/11/22
+ */
 public class InvalidPasswordException extends Exception {
-    /** InvalidPasswordException
-     * throw this when a user tries to login but the password doesn't match the username
-     *
-     */
     public InvalidPasswordException() {
         super("INCORRECT PASSCODE");
     }
