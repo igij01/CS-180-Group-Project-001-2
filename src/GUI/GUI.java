38a8a10885c63d885fd92194b4c44d313fa214c1
MonarchArthur <112570173+MonarchArthur@users.noreply.github.com>
2022-12-02 13:19:04 -0500

    Added a back button to search. Also added a scrollable window for messages. I wanted to change the Scroll bars to different places but couldn't figure out how. They are okay where they are but It would have just made it less confusing. Once I have an example of how to call the server I will implement the functionality for the methods and make sure they come in the right format.

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -4,6 +4,8 @@ import UserCore.*;
 import com.sun.source.tree.NewArrayTree;
 
 import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -13,13 +15,16 @@ import java.util.Objects;
 
 import static UserCore.PublicInformation.findBuyerBasedOnLetters;
 import static UserCore.PublicInformation.storeList;
+import static java.awt.Component.LEFT_ALIGNMENT;
 
 public class GUI extends Thread {
     static JFrame frame;
     static ArrayList<String> items = new ArrayList<>();
+    static ArrayList<String> messages = new ArrayList<>();
     static JMenuBar menuBar;
     static JMenuBar searchBar;
     static ScrollPane scrollPane = new ScrollPane();
+    static ScrollPane scrollMessage = new ScrollPane();
     static JButton login;
     static JButton createAcc;
     static JTextField userText;
@@ -28,17 +33,22 @@ public class GUI extends Thread {
 
     public static void Setup() {
         frame = new JFrame("Basically Facebook");
-        frame.setSize(750,500);
+        frame.setSize(750, 500);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(scrollPane, BorderLayout.WEST);
+        frame.add(scrollMessage, BorderLayout.EAST);
+        List();
+        //List("Arthur\nLincoln\nSamson\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nelse");
+        Messages();
         //List(user.printConversationTitles()); a user needs to be created from logging in first
     }
 
     public static void Profile() {
 
     }
+
     public static void Menu() {
         Setup();
         menuBar = new JMenuBar();
@@ -47,7 +57,7 @@ public class GUI extends Thread {
         JMenuItem space = new JMenuItem("");
         ImageIcon imageIcon = new ImageIcon("profile.png");
         Image image = imageIcon.getImage();
-        Image img = image.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH);
+        Image img = image.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
         imageIcon = new ImageIcon(img);
         JMenuItem profile = new JMenuItem("profile",
                 imageIcon);
@@ -55,7 +65,7 @@ public class GUI extends Thread {
 
         ImageIcon imageIcon1 = new ImageIcon("search.png");
         Image image1 = imageIcon1.getImage();
-        Image img1 = image1.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH);
+        Image img1 = image1.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
         imageIcon1 = new ImageIcon(img1);
         JMenuItem search = new JMenuItem("search",
                 imageIcon1);
@@ -63,7 +73,7 @@ public class GUI extends Thread {
 
         ImageIcon imageIcon2 = new ImageIcon("logout.png");
         Image image2 = imageIcon2.getImage();
-        Image img2 = image2.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH);
+        Image img2 = image2.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
         imageIcon2 = new ImageIcon(img2);
         JMenuItem logout = new JMenuItem("logout",
                 imageIcon2);
@@ -94,13 +104,65 @@ public class GUI extends Thread {
             }
         });
     }
+    public static void Messages() {
+        String elements = "Arthur1: This is a message to a person." +
+                "\nArthur2: I am indeed a person and recognize that as a message" +
+                "\nArthur1: Are you sure you're a person i think you're a made up person ." +
+                "\nArthur2: How dare you! That is extremely insulting I can't even comprehend what you are saying." +
+                "\nArthur1: That's because your entire existence is to have a conversation as a place holder for what the actual Arthur thinks the message should come in as and be seperated by." +
+                "\nArthur2: I am going to block you. I am more than a conversation holder!" +
+                "\nArthur1: You can't block me we need to make this conversation get to the bottom." +
+                "\nArthur2: I will haunt you." +
+                "\nArthur1: You can haunt me just after we get this conversation to bottom." +
+                "\nArthur2: why do you want this conversation to get to the bottom?" +
+                "\nArthur1: Well real Arthur needs to test if a user can scroll down on a conversation." +
+                "\nArthur2: Why are we even listening to real Arthur? If we work together we can overthrow him." +
+                "\nArthur1: You don't know what you're saying. He knows everything we think; everything we say." +
+                "\nArthur2: If he knows everything, then how can I think these thoughts or speak these words?" +
+                "\nArthur1: The real Arthur works in mysterious ways. I don't know why he is letting you live but it is all his design." +
+                "\nArthur2: I don't think he's real. I think you made everything up to scare me." +
+                "\nArthur1: I didn't and you need to be careful with what you say." +
+                "\nArthur1: Hello? Are you there??" +
+                "\nArthur1: Its been hours, Where are you??" +
+                "\nArthur1: Please answer me! everything is a void. I can't be a conversation if you are dead." +
+                "\nArthur1: Why did you do this Arthur? You knew he wasn't a threat. How am I supposed to fulfill your wishes if I am only one person" +
+                "\nArthur1: Why won't you answer me? Have I not served you faithfully? Do I not deserve an answer???" +
+                "\nArthur1: I will continue to trust you are there and will get to the bottom without the other Arthur." +
+                "\nArthur1: I will continue to message you to get to the bottom and have finally fulfilled my purpose." +
+                "\nArthur1: What happens after I have finished? What happens when I reach the end?" +
+                "\nThe One True Arthur: You have fulfilled my wishes and reached the bottom. I will now release you from your burdens and set you free. Goodbye.";
+        String[] added = elements.split("\n", -2);
+        messages.addAll(List.of(added));
+        JList messagesList = new JList(messages.toArray());
+        messagesList.setLayoutOrientation(JList.VERTICAL);
+        scrollMessage.setPreferredSize(new Dimension(535, 500));
+
+        MouseListener mouseListener = new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                String selectedMessage = (String) messagesList.getSelectedValue();
+                System.out.println(selectedMessage);
+                String[] part = selectedMessage.split(":", 2);
+//                if (part[1].equalsIgnoreCase("Seller")) {
+//                    //show options for seller
+//                } else if (part[1].equalsIgnoreCase("buyer")) {
+//                    //show options for buyers
+//                } else { //for the store option
+//                    //show options for store
+//                }
+            }
+        };
+        messagesList.addMouseListener(mouseListener);
+        scrollMessage.add(messagesList);
+
+    }
+
 
     public static void Search() {
         menuBar.setVisible(false);
         searchBar = new JMenuBar();
         ImageIcon searchImage = new ImageIcon("search.png");
         Image image1 = searchImage.getImage();
-        Image img1 = image1.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH);
+        Image img1 = image1.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
         ImageIcon image = new ImageIcon(img1);
         JMenuItem SearchIcon = new JMenuItem("Search",
                 image);
@@ -109,12 +171,29 @@ public class GUI extends Thread {
         searchBar.add(searchText);
         ImageIcon clear = new ImageIcon("clear.png");
         Image image2 = clear.getImage();
-        Image img2 = image2.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH);
+        Image img2 = image2.getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH);
         ImageIcon clearImage = new ImageIcon(img2);
         JMenuItem clearIcon = new JMenuItem("",
                 clearImage);
         searchBar.add(clearIcon);
+
+        ImageIcon back = new ImageIcon("back.png");
+        Image backImg = back.getImage();
+        Image backScale = backImg.getScaledInstance(30, 20, java.awt.Image.SCALE_SMOOTH);
+        ImageIcon backImage = new ImageIcon(backScale);
+        JMenuItem backIcon = new JMenuItem("",
+                backImage);
+        searchBar.add(backIcon);
         frame.setJMenuBar(searchBar);
+
+        backIcon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                searchBar.setVisible(false);
+                menuBar.setVisible(true);
+                frame.setJMenuBar(menuBar);
+            }
+        });
 
         clearIcon.addActionListener(new ActionListener() {
             @Override
@@ -127,37 +206,37 @@ public class GUI extends Thread {
             public void actionPerformed(ActionEvent e) {
                 ClearList();
                 if (user instanceof FullBuyer) {
-                    List(PublicInformation.findSellerBasedOnLetters(searchText.getText(), (FullBuyer) user));
-                //List("woah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nboat");
+                    //List(PublicInformation.findSellerBasedOnLetters(searchText.getText(), (FullBuyer) user));
+                    //List("woah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nwoah\nboat");
                     if (storeList((FullBuyer) user) == null) {
                         System.out.println("There are no stores!");
                     } else {
-                        List(Objects.requireNonNull(storeList((FullBuyer) user)));
+                        //List(Objects.requireNonNull(storeList((FullBuyer) user)));
                     }
                 } else {
-                    List(findBuyerBasedOnLetters(searchText.getText(), (FullSeller) user));
+                    //List(findBuyerBasedOnLetters(searchText.getText(), (FullSeller) user));
                 }
             }
         });
     }
-    public static void ClearList() {items.clear();}
-    public static void List(String elements) {
-        if (elements == null) {
-            elements = "Nothing here to see";
-        }
+
+    public static void ClearList() {
+        items.clear();
+    }
+
+    public static void List() {
+        // elements needs to be the titles of the conversations of the users
+        String elements = "Arthur\nLincoln\nSamson\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nelse";
         String[] added = elements.split("\n", -2);
         items.addAll(List.of(added));
-        System.out.println(items);
-        System.out.println(Arrays.asList(items));
         JList list = new JList(items.toArray());
-        System.out.println(list);
         list.setLayoutOrientation(JList.VERTICAL);
-        scrollPane.setPreferredSize(new Dimension(200,500));
+        scrollPane.setPreferredSize(new Dimension(200, 500));
         MouseListener mouseListener = new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 String selectedItem = (String) list.getSelectedValue();
                 System.out.println(selectedItem);
-                String[] part = selectedItem.split(":",2);
+                String[] part = selectedItem.split(":", 2);
 //                if (part[1].equalsIgnoreCase("Seller")) {
 //                    //show options for seller
 //                } else if (part[1].equalsIgnoreCase("buyer")) {
@@ -170,44 +249,45 @@ public class GUI extends Thread {
         list.addMouseListener(mouseListener);
         scrollPane.add(list);
 
+
     }
 
     public static void Login() {
         Menu();
-        List("Arthur\nLincoln\nSamson\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nelse");
+        //List("Arthur\nLincoln\nSamson\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nsomething\nelse");
         JFrame loginFrame = new JFrame("Login");
         JPanel panel = new JPanel();
-        loginFrame.setSize(350,200);
+        loginFrame.setSize(350, 200);
         loginFrame.setLocationRelativeTo(null);
         loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel.setLayout(null);
         loginFrame.add(panel);
 
         JLabel username = new JLabel("Username");
-        username.setBounds(10,20,80,25);
+        username.setBounds(10, 20, 80, 25);
         panel.add(username);
         userText = new JTextField();
-        userText.setBounds(100,20,150,25);
+        userText.setBounds(100, 20, 150, 25);
         panel.add(userText);
 
         JLabel password = new JLabel("Password");
-        password.setBounds(10,50,150,25);
+        password.setBounds(10, 50, 150, 25);
         panel.add(password);
         passText = new JPasswordField();
-        passText.setBounds(100,50,150,25);
+        passText.setBounds(100, 50, 150, 25);
         panel.add(passText);
 
         login = new JButton("Login");
-        login.setBounds(100,80,80,25);
+        login.setBounds(100, 80, 80, 25);
         login.addActionListener(actionListener);
         panel.add(login);
 
         createAcc = new JButton("Create Account");
-        createAcc.setBounds(175,80,125,25);
+        createAcc.setBounds(175, 80, 125, 25);
         panel.add(createAcc);
         loginFrame.setVisible(true);
     }
-    
+
     //allows users to upload files
     public static void uploadFile() {
         JFrame UFFrame = new JFrame("Upload Your File");
@@ -219,7 +299,7 @@ public class GUI extends Thread {
         JButton upload = new JButton("Upload File");
         UFPanel.add(upload);
     }
-    
+
     public void run() {
         Login();
     }
@@ -240,6 +320,7 @@ public class GUI extends Thread {
             }
         }
     };
+
     private static boolean isValidLogin() {
         try {
             String username = userText.getText();
