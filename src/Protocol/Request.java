b115861a885feef425151f56f720e6a31a8cdb47
Samson Tesfagiorgis <stesfag@purdue.edu>
2022-12-03 15:42:06 -0500

    Profile Menu Button added

--- src/Protocol/Request.java
+++ src/Protocol/Request.java
@@ -3,7 +3,8 @@ package Protocol;
 public enum Request {
     LOGIN (2),
     REGISTER (4),
-    DISPLAY_PROFILE (0);
+    DISPLAY_PROFILE (0),
+    CHANGE_USERNAME(2);
 
     public final int paramCount;
     private Request(int paramCount) {
